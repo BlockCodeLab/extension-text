1d49bd00afd10c8acf3a7e91fe75cdd698231f37
Yeqin Gong <gon@gyeq.in>
2024-10-30 23:55:13 +0800

    update tags

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export default {
   description: 'Bring words to life.',
   image: featureImage,
   icon: iconImage,
-  tags: ['blocks', 'arcade', 'display'],
+  tags: ['arcade'],
 
   // l10n
   translations: {
